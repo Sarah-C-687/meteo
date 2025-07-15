55ca269330bceaf7f99c7b8ba3cca064df957ae1
Sarah Crèvecoeur <sarah.crevecoeur.pro@gmail.com>
2025-07-14 20:08:38 -0400

    Injected weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,41 @@ function refreshWeather(response){
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#weather-city");
+  let timeElement = document.querySelector("#current-time");
+  let conditionElement = document.querySelector("#condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let weatherIconElement = document.querySelector("#weather-icon");
 
+  let date = new Date(response.data.time * 1000);
+  
   cityElement.innerHTML = response.data.city ;
+  timeElement.innerHTML = formatDate(date);
+  conditionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windSpeedElement.innerHTML = response.data.wind.speed;
   temperatureElement.innerHTML = Math.round(temperature);
+  
+  console.log(response.data);
+}
+
+function formatDate(date){
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days=[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city){

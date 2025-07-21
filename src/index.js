7e78f87dcf3e4875769e06182ffd768a4221c5b9
Sarah Crèvecoeur <sarah.crevecoeur.pro@gmail.com>
2025-07-21 16:36:58 -0400

    Corrected display

--- src/index.js
+++ src/index.js
@@ -77,7 +77,7 @@ function displayForecast(response) {
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
-   if (index > 0 && index < 5) {
+   if (index > 0 && index < 6) {
     forecastHtml = 
       forecastHtml +
       `
